914241129afe0a254a138645c0ea75e5a2d39875
Jesper Eskilson <jesper.eskilson@klarna.com>
2024-12-02 08:49:53 +0100

    day 2 part 2

--- day2.erl
+++ day2.erl
@@ -13,51 +13,68 @@ solve0(Bin) ->
   Lines = binary:split(Bin, <<"\n">>, [global]),
   P1 =
     lists:foldl(fun(<<>>, Acc) -> Acc;
-                   (Line, Acc) ->
+                   (Line, {Acc0, Acc1}) ->
                     Report = lists:map(fun binary_to_integer/1, binary:split(Line, <<" ">>, [global])),
-                    io:format(">> ~w ", [Report]),
-                    case is_safe(Report) of
-                      true ->
-                        io:format(" [SAFE]~n", []),
-                        1 + Acc;
-                      _ ->
-                        io:format("~n", []),
-                        Acc
-                    end
-                end, 0, Lines),
+                    {Acc0 + is_safe(Report), Acc1 + is_safe2(Report)}
+                end, {0, 0}, Lines),
   P1.
+
+%% Part 1
 
 is_safe([A, B|_] = Report) when A < B ->
   is_safe_incr(Report);
 is_safe([A, B|_] = Report) when A > B ->
   is_safe_decr(Report);
 is_safe(_) ->
-  false.
+  0.
 
-is_safe_incr([_]) -> true;
+is_safe_incr([_]) -> 1;
 is_safe_incr([A, B|Rest]) when B - A >= 1 andalso B - A =< 3 ->
   is_safe_incr([B|Rest]);
 is_safe_incr(_) ->
-  false.
+  0.
 
-is_safe_decr([_]) -> true;
+is_safe_decr([_]) -> 1;
 is_safe_decr([A, B|Rest]) when A - B >= 1 andalso A - B =< 3 ->
   is_safe_decr([B|Rest]);
 is_safe_decr(_) ->
-  false.
+  0.
 
+%% Part 2
+
+%% Brute-force this to avoid various corner cases by generating all
+%% lists where we delete one item at a time and check if the resulting
+%% list is safe.
+is_safe2(Report) ->
+  SkipIndexes = lists:seq(0, length(Report) - 1),
+  case lists:any(fun(L) -> is_safe(L) == 1 end,
+                 lists:map(fun(Idx) ->
+                               delete_at(Idx, Report)
+                           end, SkipIndexes)) of
+    true -> 1;
+    false -> 0
+  end.
+
+delete_at(Idx, List) ->
+  {L1, [_|L2]} = lists:split(Idx, List),
+  L1 ++ L2.
 
 -ifdef(TEST).
-is_safe_test() ->
-  ?assert(is_safe_incr([1,2])),
-  ?assert(is_safe([1,2,3])).
-
 ex1_test() ->
-  ?assert(is_safe([7,6,4,2,1])),
-  ?assertNot(is_safe([1,2,7,8,9])),
-  ?assertNot(is_safe([9,7,6,2,1])),
-  ?assertNot(is_safe([1,3,2,4,5])),
-  ?assertNot(is_safe([8,6,4,4,1])),
-  ?assert(is_safe([1,3,6,7,9])).
+  ?assertEqual(1, is_safe([7,6,4,2,1])),
+  ?assertEqual(0, is_safe([1,2,7,8,9])),
+  ?assertEqual(0, is_safe([9,7,6,2,1])),
+  ?assertEqual(0, is_safe([1,3,2,4,5])),
+  ?assertEqual(0, is_safe([8,6,4,4,1])),
+  ?assertEqual(1, is_safe([1,3,6,7,9])).
+
+ex2_test() ->
+  ?assertEqual(1, is_safe2([7,6,4,2,1])),
+  ?assertEqual(0, is_safe2([1,2,7,8,9])),
+  ?assertEqual(0, is_safe2([9,7,6,2,1])),
+  ?assertEqual(1, is_safe2([1,3,2,4,5])),
+  ?assertEqual(1, is_safe2([8,6,4,4,1])),
+  ?assertEqual(1, is_safe2([1,3,6,7,9])).
+
 
 -endif.
